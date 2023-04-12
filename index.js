79afbcecf7a0eb7fe1db0f1540ea7413df660e2c
Penn Curtis <penn.s.m.c@gmail.com>
2023-04-12 10:08:29 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = {
+    name : "name",
+    streetAdress: "address"
+}
+
+
+function updateEmployeeWithKeyAndValue(employee, name, value){
+    const newEmployee = {...employee}
+    newEmployee[name] = value
+    return newEmployee
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, name, value){
+    employee[name] = value
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, name){
+    const newEmployee = {...employee}
+    delete newEmployee.name
+    return newEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, name){
+    delete employee.name
+    return employee
+}
